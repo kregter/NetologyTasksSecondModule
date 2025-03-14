3c029a2b03720d31e6a2869d16cbd521ce74f93c
kregter <pihteevn@ru-drive.com>
2025-03-15 00:40:43 +0300

    Add homework 7.2

--- 7/2/homework7.2.cpp
+++ 7/2/homework7.2.cpp
@@ -1,1 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <windows.h>
+#include <string>
+
+#define SUB(a,b) ((a)-(b)) 
+
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+		
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
+}
+
 
